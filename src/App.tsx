4e56e785fa4c2165a06b9a410d44767b5a849bb3
antony-akshay <akshay.dev.ac.in@gmail.com>
2026-02-22 22:23:10 +0530

    app_setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+    <div className=' h-screen text-8xl text-blue-600 flex items-center justify-center'>
+      decision companion
+    </div>
+  )
+}
+
+export default App
